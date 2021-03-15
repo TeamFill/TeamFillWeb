49c878d69e8a1183e9dd624554f5d9ba332bbd17
kajoban <kuhapark@mcmaster.ca>
2021-03-15 18:56:09 -0400

    updates

--- src/src/App.js
+++ src/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import "antd/dist/antd.css";
 import Profile from "./containers/Profile";
 
 function App() {
