384e427df0c621c37599682b93d7d0915aa49f1c
kajoban <kuhapark@mcmaster.ca>
2021-03-15 18:08:53 -0400

    wip: started profile page

---
+++ src/src/containers/Default.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Default() {
+  return (
+    <div>
+      <p>Welcome to TeamFill!</p>
+    </div>
+  );
+}
---
+++ src/src/containers/Profile.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Typography, Form, Input, Button, Checkbox } from "antd";
+
+const { Title } = Typography;
+
+export default function Profile() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div>
+      <Title>My Profile</Title>
+      <Form
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item name="remember" valuePropName="checked">
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+}
