dda538012cd0b7d3931ae77c585915c9cd149e56
kajoban <kuhapark@mcmaster.ca>
2021-03-17 09:50:33 -0400

    moved custom popup to component

--- src/components/EventPopup.js
+++ src/components/EventPopup.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { Typography, Button } from "antd";
 
-export default function EventPopup() {
-  return;
+const { Text, Paragraph } = Typography;
+
+export default function EventPopup(props) {
+  const handleClick = () => alert("nice click");
+  return (
+    <>
+      <Paragraph>
+        <Text strong>{props.event.properties.name}</Text>
+      </Paragraph>
+      <Paragraph>
+        {props.event.properties.date} at {props.event.properties.time}
+      </Paragraph>
+      <Button
+        style={{
+          width: "100%",
+          height: 30,
+          borderRadius: 15,
+          borderColor: "#ff5252",
+          backgroundColor: "#ff5252",
+        }}
+        type="primary"
+        onClick={() => handleClick()}
+      >
+        More info
+      </Button>
+    </>
+  );
 }
--- src/containers/HomepageMap.js
+++ src/containers/HomepageMap.js
@@ -2,12 +2,10 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
-import { Typography, Button } from "antd";
+import EventPopup from "../components/EventPopup";
 import * as eventData from "../data/test-event-data.json";
 
 const MapboxURL = `https://api.mapbox.com/styles/v1/${process.env.REACT_APP_MAPBOX_USERNAME}/${process.env.REACT_APP_MAPBOX_STYLE_ID}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`;
-
-const { Text, Paragraph } = Typography;
 
 const StyledPopup = styled(Popup)`
   background: #ffffff;
@@ -21,7 +19,6 @@ const icon = new Icon({
 });
 
 export default function HomepageMap() {
-  const handleClick = () => alert("nice click");
   return (
     <MapContainer center={[43.2609, -79.9192]} zoom={15}>
       <TileLayer url={MapboxURL} />
@@ -35,25 +32,7 @@ export default function HomepageMap() {
           icon={icon}
         >
           <StyledPopup>
-            <Paragraph>
-              <Text strong>{event.properties.name}</Text>
-            </Paragraph>
-            <Paragraph>
-              {event.properties.date} at {event.properties.time}
-            </Paragraph>
-            <Button
-              style={{
-                width: "100%",
-                height: 30,
-                borderRadius: 15,
-                borderColor: "#ff5252",
-                backgroundColor: "#ff5252",
-              }}
-              type="primary"
-              onClick={() => handleClick()}
-            >
-              More info
-            </Button>
+            <EventPopup event={event} />
           </StyledPopup>
         </Marker>
       ))}
