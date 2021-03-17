18e9110eb422b6b501fc3eb83467e62f5d5fe972
kajoban <kuhapark@mcmaster.ca>
2021-03-17 09:13:26 -0400

    added custom svg map icon

--- src/containers/HomepageMap.js
+++ src/containers/HomepageMap.js
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
 import * as eventData from "../data/test-event-data.json";
 
 const MapboxURL = `https://api.mapbox.com/styles/v1/${process.env.REACT_APP_MAPBOX_USERNAME}/${process.env.REACT_APP_MAPBOX_STYLE_ID}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`;
+
+const icon = new Icon({
+  iconUrl: "/map-marker.svg",
+  iconSize: [40, 40],
+});
 
 export default function HomepageMap() {
   return (
@@ -15,6 +21,7 @@ export default function HomepageMap() {
             event.geometry.coordinates[0],
             event.geometry.coordinates[1],
           ]}
+          icon={icon}
         >
           <Popup>{event.properties.name}</Popup>
         </Marker>
