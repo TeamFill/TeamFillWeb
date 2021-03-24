0ae2064d6749ac26f01ea7fe05a41f85205e5fe6
kajoban <kuhapark@mcmaster.ca>
2021-03-24 18:00:03 -0400

    fixed rerouting to myevents based on original route

--- src/components/EventPopup.js
+++ src/components/EventPopup.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Typography, Button } from "antd";
+import { NavLink } from "react-router-dom";
 
 const { Text, Paragraph } = Typography;
 
 export default function EventPopup(props) {
   const handleClick = () => alert("nice click");
   return (
-    <>
+    <NavLink
+      to={{
+        pathname: "/eventinfo",
+        aboutProps: {
+          title: props.event.properties.name,
+          time: props.event.properties.time,
+          ball: "soccer",
+          returnTo: "/home",
+        },
+      }}
+      exact
+    >
       <Paragraph>
         <Text strong>{props.event.properties.name}</Text>
       </Paragraph>
@@ -22,10 +34,13 @@ export default function EventPopup(props) {
           backgroundColor: "#ff5252",
         }}
         type="primary"
-        onClick={() => handleClick()}
+        onClick={(e) => {
+          e.preventDefault();
+          handleClick();
+        }}
       >
-        More info
+        Request to Join
       </Button>
-    </>
+    </NavLink>
   );
 }
