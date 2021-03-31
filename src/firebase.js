7b72fcb6a20cc372de3305cc80c12029e549a5ae
kajoban <kuhapark@mcmaster.ca>
2021-03-30 22:44:49 -0400

    moved keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const fb = firebase.initializeApp({
-  apiKey: "AIzaSyBt3nOdu4Q1A_S3nu8yGHlo_a-Ye56hktA",
-  authDomain: "teamfill-f2b20.firebaseapp.com",
-  projectId: "teamfill-f2b20",
-  storageBucket: "teamfill-f2b20.appspot.com",
-  messagingSenderId: "process.env.MESSAGING_SENDER_ID",
-  appId: "1:12323763387:web:b297215a1e40d80a2f659e",
-  measurementId: "G-measurement-id",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 });
 
 export default fb;
