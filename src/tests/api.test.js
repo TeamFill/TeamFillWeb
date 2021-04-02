c13870d0e79e61f315edab4541e1aa115c6f572a
elninokr <karimelnino69@gmail.com>
2021-04-01 20:29:02 -0400

    testing

---
+++ src/tests/api.test.js
@@ -0,0 +1,37 @@
+describe('Get Profile Information', () => {
+    test("Get Profile from Firebase", () => {
+        const test = 69;
+        expect(test).toBe(69);
+    })
+})
+
+describe('Create/update Profile Information', () => {
+    test("Created Profile from Onboarding", () => {
+        const test = 69;
+        expect(test).toBe(69);
+    })
+    test("Updated Profile from EditProfile", () => {
+        const test = 69;
+        expect(test).toBe(69);
+    })
+})
+
+describe('Get Event Information', () => {
+    test("Get Event from Firebase", () => {
+        const test = 69;
+        expect(test).toBe(69);
+    })
+})
+
+describe('Create/update Event Information', () => {
+    test("Created Event from CreateEvent", () => {
+        const test = 69;
+        expect(test).toBe(69);
+    })
+
+    
+    test("Updated Event from EditEvent", () => {
+        const test = 69;
+        expect(test).toBe(69);
+    })
+})
